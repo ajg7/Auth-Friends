4c796219cee66aa91f607c572246fcc385d06d30
ajg7 <ajgebara@gmail.com>
2020-09-15 23:28:24 -0500

    made a submit and change function in the login component

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,22 +1,45 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import axios from "axios";
+import { axiosWithAuth } from "../axiosWithAuth";
 
 const Login = props => {
 
+    const credentials = {
+        username: "",
+        password: ""
+    }
+
     const history = useHistory();
-    
+    const [loginValues, setLoginValues] = useState(credentials)
+
+    const change = event => {
+        setLoginValues({...loginValues, [event.target.name]: event.target.value})
+    }
+
+    const submitLogin = event => {
+        event.preventDefault();
+        axiosWithAuth().post("http://localhost:5000/api/login", loginValues)
+            .then(response => {
+                localStorage.setItem("token", response.data.payload);
+                history.push("/protected");
+            })
+            .catch(error => {
+                console.log(error)
+            })
+
+    }
 
     return(
         <div className="login">
             <h2>Login</h2>
-            <form>
+            <form onSubmit={submitLogin}>
                 <label> Username:
                     <input 
                     name="username"
                     type="text"
                     placeholder="Enter Username Here..."
-                    onChange={}
+                    onChange={change}
                     />
                 </label>
                 <label> Password:
@@ -24,7 +47,7 @@ const Login = props => {
                     name="password"
                     type="password"
                     placeholder="Enter Password Here..."
-                    onChange={}
+                    onChange={change}
                     />
                 </label>
                 <button>Login</button>
